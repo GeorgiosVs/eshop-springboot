1367d64b83c1e7cf169ba43f7acc444de7dc5909
Georgios <158276611+GeorgiosVs@users.noreply.github.com>
2025-06-09 21:13:33 +0300

    Delete eshop directory

--- eshop/src/main/java/com/example/eshop/domainLayer/repositories/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.eshop.domainLayer.repositories;
-
-import com.example.eshop.domainLayer.entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, String>{ }
